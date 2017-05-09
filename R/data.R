73a317c2e56313d8ba0c07f1daee5a09c828d97f
Seborinos <sebastianteranhidalgo@gmail.com>
2017-05-08 21:35:51 -0400

    I added a file documentating the data. Not yet constructed though

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Data on copy number aberrations.
+#'
+#' A dataset containing CNAs from TCGA
+#'
+#' @format A data frame with n rows and p variables:
+#' \describe{
+#'   \item{variable1}{variable1 is something}
+#'   \item{variable2}{variable2 is something else}
+#'   ...
+#' }
+#' @source \url{http:}
+"brca.data.cna"
