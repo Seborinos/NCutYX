824907e1c8c4e63fc0d421b2fc41df53ca96de63
Seborinos <sebastianteranhidalgo@gmail.com>
2017-10-10 15:50:12 -0400

    Added some other helper functions

--- R/helperfunctions.R
+++ R/helperfunctions.R
@@ -11,42 +11,33 @@ neovector <- function(Probs){
   return(rbinom(K,1,prob=Probs))
 }
 
+
 #rewrite the function below in C++
 #Also maybe you need to scale the weights so that they equal 1
-w.gaussian <- function(Z,Y,X,sigma2=1){
+w.gauss <- function(Z,Y,X,sigma=1){
   n <- dim(X)[1]
   q <- dim(Z)[2]
   p <- dim(Y)[2]
   r <- dim(X)[2]
   m <- q + p + r
-  Dzy <- matrix(0,q,p)
-  Dyx <- matrix(0,p,r)
   D <- matrix(0,m,m)
-  for (i in 1:q){
-    for (j in 1:p){
-      Dzy[i,j] <- exp((-1)*sum((Z[,i]-Y[,j])^2)/sigma2)#provfis said this slow, do C++ instead
-      Dzy[j,i] <- Dzy[i,j]
-    }
-  }
+  A <- exp((-sigma)*as.matrix(dist(t(cbind(Z,Y,X)),
+                                   method='euclidean',
+                                   diag=T,upper=T)))
 
-  for (i in 1:p){
-    for (j in 1:r){
-      Dyx[i,j] <- exp((-1)*sum((Y[,i]-X[,j])^2)/sigma2)#provfis said this slow, do C++ instead
-      Dyx[j,i] <- Dyx[i,j]
-    }
-  }
-
-  D[1:q,(q+1):(q+p)] <- Dzy
-  D[(q+1):(q+p),1:q] <- t(Dzy)
-  D[(q+1):(q+p),(q+p+1):(q+p+r)] <- Dyx
-  D[(q+p+1):(q+p+r),(q+1):(q+p)] <- t(Dyx)
+  D[1:q,(q+1):(q+p)] <- A[1:q,(q+1):(q+p)]
+  D[(q+1):(q+p),1:q] <- t(D[1:q,(q+1):(q+p)])
+  D[(q+1):(q+p),(q+p+1):(q+p+r)] <- A[(q+1):(q+p),(q+p+1):(q+p+r)]
+  D[(q+p+1):(q+p+r),(q+1):(q+p)] <- t(D[(q+1):(q+p),(q+p+1):(q+p+r)] )
   return(D)
 }
 
 
 #rewrite the function below in C++
 #Also maybe you need to scale the weights so that they equal 1
-w.cor <- function(Z,Y,X){
+w.cor <- function(Z,
+                  Y,
+                  X){
   n <- dim(X)[1]
   q <- dim(Z)[2]
   p <- dim(Y)[2]
